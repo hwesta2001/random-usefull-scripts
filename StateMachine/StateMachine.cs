e56e90af0abe776cf59587e76e89dba1c11f2e28
hwesta2001 <79084110+hwesta2001@users.noreply.github.com>
2021-04-18 14:36:30 +0300

    Add files via upload

---
+++ StateMachine/StateMachine.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class StateMachine : MonoBehaviour
+{
+    private IState currentlyRunningState;
+    private IState previousState;
+
+    public void ChangeState(IState newState)
+    {
+        if (currentlyRunningState != null)
+        {
+            this.currentlyRunningState.Exit();
+        }
+        this.previousState = this.currentlyRunningState;
+        this.currentlyRunningState = newState;
+        this.currentlyRunningState.Enter();
+    }
+
+    public void ExecuteStateUpdate()
+    {
+        var runningState = this.currentlyRunningState;
+        if (runningState != null)
+        {
+            runningState.Execute();
+        }
+    }
+
+    public void SwitchToPreviousState()
+    {
+        this.currentlyRunningState.Exit();
+        this.currentlyRunningState = this.previousState;
+        this.currentlyRunningState.Enter();
+    }
+}
