726f9130f900cf548ae99c3e36225be15c7d5e6d
Hasan YILMAZ <79084110+hwesta2001@users.noreply.github.com>
2023-09-05 21:52:01 +0300

    Create LeaderBoard.cs

---
+++ LeaderBoard.cs
@@ -0,0 +1,26 @@
+
+using System;
+
+public class LeaderBoard : Monobehaivour
+{
+	
+	public List<Score>  scores = new() ;
+	
+	void CompareScores(Score _score) 
+	{
+		for (int i=0; i<scores.Count; i++) 
+		{
+			if(_score.score>=scores[i])
+			
+		} 
+	
+	} 
+	
+	
+}
+
+public class Score
+{
+	public int score;
+	public string name;
+} 
