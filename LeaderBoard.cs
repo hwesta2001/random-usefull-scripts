c10976e65cfb02df2e2a4cf6b928c254f1c6eabe
Hasan YILMAZ <79084110+hwesta2001@users.noreply.github.com>
2023-09-06 00:01:42 +0300

    Update LeaderBoard.cs

--- LeaderBoard.cs
+++ LeaderBoard.cs
@@ -3,24 +3,47 @@ using System;
 
 public class LeaderBoard : Monobehaivour
 {
-	
-	public List<Score>  scores = new() ;
-	
-	void CompareScores(Score _score) 
-	{
-		for (int i=0; i<scores.Count; i++) 
-		{
-			if(_score.score>=scores[i])
-			
-		} 
-	
-	} 
-	
-	
+ public int currentScore;
+ public List<Score>  scores = new();
+ public List<TexMeshProOnGui> textMeshesNames = new();
+ public List<TexMeshProOnGui> textMeshesScores = new();
+
+ void SetScore(string _name)
+ {
+   currentScore=Globals.ins.Score; 
+   Score newScore = new();
+   newScore.name = _name;
+   newScore.score =currentScore;
+   CompareScores(newScore);
+ } 
+ void CompareScores(Score _score) 
+ {
+  for (int i=0; i<scores.Count; i++) 
+  {
+   if(_score.score>=scores[i])
+   {
+    scores[i]=_score;
+    break;
+   }
+  }
+  RefresLeaderboardList();
+ }
+
+void RefresLeaderboardList()
+{
+ for(int i = 0; i<scores.Count; i++) 
+ {
+   textMeshesNames[i].text=scores[i].name;
+   textMeshesScores[i].text=scores[i].score;
+ } 
+
+
+} 
+ 
 }
 
 public class Score
 {
-	public int score;
-	public string name;
+ public int score;
+ public string name;
 } 
