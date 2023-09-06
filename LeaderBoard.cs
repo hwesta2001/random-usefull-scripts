8ec5db2285d04ec0b3fbc7eb2ec0479b24c2b81d
Hasan YILMAZ <79084110+hwesta2001@users.noreply.github.com>
2023-09-06 18:28:53 +0300

    Update LeaderBoard.cs

--- LeaderBoard.cs
+++ LeaderBoard.cs
@@ -1,50 +1,59 @@
+
+using System.Collections.Generic;
 using UnityEngine;
-using System;
-
-public class LeaderBoard : Monobehaivour
-{
- public int currentScore;
- public List<Score>  scores = new(); // new değil playerprefaba yaz çek.
-                                     // ilk başta custom bir liste oluşturabiliriz 
- public List<TexMeshProOnGui> textMeshesNames = new();
- public List<TexMeshProOnGui> textMeshesScores = new();
-
- void SetScore(string _name)
- {
-   currentScore=Globals.ins.Score; 
-   Score newScore = new();
-   newScore.name = _name;
-   newScore.score =currentScore;
-   CompareScores(newScore);
- } 
- void CompareScores(Score _score) 
- {
-  for (int i=0; i<scores.Count; i++) 
-  {
-   if(_score.score>=scores[i])
-   {
-    scores[i]=_score;
-    break;
-   }
-  }
-  RefresLeaderboardList();
- }
-
-void RefresLeaderboardList()
-{
- for(int i = 0; i<scores.Count; i++) 
- {
-   textMeshesNames[i].text=scores[i].name;
-   textMeshesScores[i].text=scores[i].score;
- } 
-
-
-} 
- 
-}
-
+using TMPro;
 public class Score
 {
- public int score;
- public string name;
-} 
+    public int score;
+    public string name;
+}
+
+public class LeaderBoardManager : MonoBehaviour
+{
+
+    public class LeaderBoard : MonoBehaviour
+    {
+        public int currentScore;
+        public List<Score> scores = new(); // new değil playerprefaba yaz çek.
+                                           // ilk başta custom bir liste oluşturabiliriz 
+        public List<TextMeshProUGUI> textMeshesNames = new();
+        public List<TextMeshProUGUI> textMeshesScores = new();
+
+        void SetScore(string _name)
+        {
+            currentScore = GlobalVeriler.ins.Score;
+            Score newScore = new()
+            {
+                name = _name,
+                score = currentScore
+            };
+            CompareScores(newScore);
+        }
+
+        void CompareScores(Score _score)
+        {
+            for (int i = 0; i < scores.Count; i++)
+            {
+                if (_score.score >= scores[i].score)
+                {
+                    scores[i] = _score;
+                    break;
+                }
+            }
+            RefresLeaderboardList();
+        }
+
+        void RefresLeaderboardList()
+        {
+            for (int i = 0; i < scores.Count; i++)
+            {
+                textMeshesNames[i].text = scores[i].name;
+                textMeshesScores[i].text = scores[i].score.ToString();
+            }
+
+        }
+
+    }
+
+
+}
